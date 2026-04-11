6b376aac74200c33428a5cc6ac26ef28949ddcc9
Shankargouda <shankargoudadesai43@gmail.com>
2026-04-11 06:50:17 +0530

    changes in db.js

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER ,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
